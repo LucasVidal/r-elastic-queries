49b9a307fde79d92f7d65fed936aa1183b8d787c
Lucas Vidal <lvidal@twilio.com>
2017-03-14 17:41:38 -0500

    removed unused function

--- output.R
+++
@@ -1,4 +0,0 @@
-# Helper method, creates empty dataframe with names
-dataframe_with_names <- function(names) {
-  as.data.frame(setNames(replicate(length(names),numeric(0), simplify = F), names), stringsAsFactors = FALSE)
-}
